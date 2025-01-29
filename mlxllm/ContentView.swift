debcb3dd6636711c4065cefdbabfc20802deb846
Jerry Zhao <jerryzhao173985@gmail.com>
2025-01-29 16:53:38 +0800

    Initial Commit

---
+++ mlxllm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mlxllm
+//
+//  Created by jerry on 1/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mlxllm/mlxllmApp.swift
@@ -0,0 +1,17 @@
+//
+//  mlxllmApp.swift
+//  mlxllm
+//
+//  Created by jerry on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct mlxllmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
